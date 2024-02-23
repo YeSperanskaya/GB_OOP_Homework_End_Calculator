3ebeaa2daa41b78809cace8cc7955e2ca47b4b22
Evgeniya Speranskaya <ye.speranskaya@mail.ru>
2024-02-23 09:07:15 +0500

    добавила изменение массива

--- src/main/java/servises/ChangeArray.java
+++ src/main/java/servises/ChangeArray.java
@@ -12,17 +12,40 @@ public class ChangeArray {
     List<Double> listNumbers = dataInput.getListNumbers();
     List<String> listCommands = dataInput.getListCommands();
 
-    public double exampleSolution() {
+    public void exampleSolution() {
         int i = 0;
-        while (listNumbers.size() > 1) {
-            if (listCommands.get(i).equals("*") || listCommands.get(i).equals("/")) {
-                if
+        double newMeaning;
+        while (listNumbers.size() > 1 && i < listNumbers.size()) {
+            if (isExistMultyDiv()) {
+                if(listCommands.get(i).equals("*")) {
+                    newMeaning = calculator.sum(listNumbers.get(i), listNumbers.get(i+1));
+                } else if (listCommands.get(i).equals("/")) {
+                    newMeaning = calculator.division(listNumbers.get(i), listNumbers.get(i+1));
+                } else {
+                    i++;
+                }
+            } else {
 
             }
         }
 
     }
 
+
+
+    private boolean isExistMultyDiv() {
+        if (listCommands.contains("*") || listCommands.contains("/")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void changeArray(int i, double newMeaning) {
+        listCommands.remove(i);
+        listNumbers.set(i, newMeaning);
+        listNumbers.remove(i+1);
+    }
 
     /**
      * пока вообще содержится * или поделить надо делать только эти действия по порядку
